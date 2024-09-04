f385b3c346e5463b57dee6b7d605b043187fb33c
azharsquared <azharsquared@gmail.com>
2024-09-04 13:21:14 +0530

    feat; github pull

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import OpenWhether from './OpenWhether.tsx'
 import './index.css'
+import Github from './github.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Github />
   </StrictMode>,
 )
